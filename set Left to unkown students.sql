16df2db8268a2f23805f10c010ee765d1db37741
Kelvin Maina <mainakelvin360@gmail.com>
2023-09-18 16:33:49 +0300

    Update set Left to unkown students.sql

--- set Left to unkown students.sql
+++ set Left to unkown students.sql
@@ -1,7 +1,12 @@
 UPDATE `gibbonPerson`
 SET `status` = 'Left'
-WHERE `gibbonRoleIDPrimary` = '003'
+WHERE `gibbonRoleIDPrimary` = 3
 AND `gibbonPersonID` NOT IN (
   SELECT DISTINCT `gibbonPersonID`
   FROM `gibbonStudentEnrolment`
-)
+  WHERE `gibbonSchoolYearID` = (
+    SELECT `gibbonSchoolYearID`
+    FROM `gibbonSchoolYear`
+    WHERE `status` = 'Current'
+  )
+);
