b737a49664acfcc7467b2e0031bb878715608c8c
Kelvin Maina <mainakelvin360@gmail.com>
2024-03-22 16:42:09 +0300

    Include updating column details

--- FormalAssessment.sql
+++ FormalAssessment.sql
@@ -2,4 +2,10 @@
 DELETE FROM gibbonInternalAssessmentColumn
 WHERE completeDate = "YYYY-MM-DD"
 
+#Rename multiple columns
+UPDATE gibbonInternalAssessmentColumn
+SET name = "NEW NAME"
+WHERE name = "OLD NAME"
+AND type = "COLUMN TYPE"
+
 #more
